3b372b7285dbaf8ce6014adb3e3ed67e85529be0
geovanigcs <geovanigcs.dev@gmail.com>
2024-10-25 20:27:50 -0300

    Feat: Add HeroSection

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,13 @@
+
+
+export const HeroSection = () => {
+    return (
+        <section>
+            <div className="container">
+                <p>Introducing Blockforge</p>
+                <h1>The future of BlackChain is Here.</h1>
+                <p>Blockforge is pioneering smart contract integrity with cutting-edge data solutions.</p>
+            </div>
+        </section>
+    )
+}
